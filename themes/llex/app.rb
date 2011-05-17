47ecd7c02d4d927b1c49ffb4f6decd7ee67a5a34
puttiz <puttiz@gmail.com>
2011-05-18 00:28:01 +0800

    compass working with /css route. project_path and sass_dir configuration can't work!?

--- themes/llex/app.rb
+++ themes/llex/app.rb
@@ -7,22 +7,19 @@ module Nesta
 
     configure do
       Compass.configuration do |config|
-        config.project_path = File.dirname(__FILE__)
-        config.sass_dir = "scss"
+        # config.project_path = File.dirname(__FILE__)
+        # config.sass_dir = "scss"
         # config.output_style = :compact
+        # config.line_comments = false
       end
-      
-      set :haml, { :format => :html5 }
-      set :sass, Compass.sass_engine_options
+    
+      set :scss, Compass.sass_engine_options
+      # set :views, File.expand_path('scss', File.dirname(__FILE__))
     end
 
-    helpers do
-      # Add new helpers here.
-    end
-
-    get '/assets/c/:name.css' do
+    get '/css/:name.css' do
       content_type 'text/css', :charset => 'utf-8'
-      sass :params[:name].to_sym
+      cache scss(params[:name].to_sym)
     end
     
   end
