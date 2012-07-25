59c6ed75f98436a67cb83c3fe16c7b969fa68bd0
puttiz <puttiz@gmail.com>
2012-07-25 18:30:48 +0800

    die

--- Gemfile
+++ Gemfile
@@ -1,12 +1,9 @@
 source :gemcutter
 
 gem 'nesta'
-gem 'heroku'
 
 gem 'haml'
-gem 'sass'
-gem 'compass'
-gem 'compass_twitter_bootstrap'
+# gem 'compass_twitter_bootstrap'
 
 group :development do
   gem 'guard-livereload'
--- config/setup_load_paths.rb
+++ config/setup_load_paths.rb
@@ -1,18 +1,12 @@
-# RVM support in Rails 3 app for multiple gemsets with Passenger
-# config/setup_load_paths.rb
-
 if ENV['MY_RUBY_HOME'] && ENV['MY_RUBY_HOME'].include?('rvm')
   begin
-    rvm_path     = File.dirname(File.dirname(ENV['MY_RUBY_HOME']))
-    rvm_lib_path = File.join(rvm_path, 'lib')
-    $LOAD_PATH.unshift rvm_lib_path
     require 'rvm'
     RVM.use_from_path! File.dirname(File.dirname(__FILE__))
   rescue LoadError
-    # RVM is unavailable at this point.
-    raise "RVM ruby lib is currently unavailable."
+    raise "RVM gem is currently unavailable."
   end
 end
 
+# If you're not using Bundler at all, remove lines bellow
 ENV['BUNDLE_GEMFILE'] = File.expand_path('../Gemfile', File.dirname(__FILE__))
 require 'bundler/setup'
